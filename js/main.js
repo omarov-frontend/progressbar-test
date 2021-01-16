0f66865fa8f95d4f6b07a2a1cacebaccca3ab055
Shamil Omarov <s_omarov05@mail.ru>
2021-01-16 20:17:41 +0300

    Сверстал прогресс-элемент + через JS сделал заполнение по времени

---
+++ js/main.js
@@ -0,0 +1,19 @@
+function userProgress(time) {
+    var start = 0;
+    var time = Math.round(time * 1000 / 100);
+    var progressElement = document.getElementById('user-progress');
+    var intervalId = setInterval(function() {
+        if(start > 100) {
+            clearInterval(intervalId);
+            userProgressCallBack();
+        } else {
+            progressElement.value = start;
+        }
+        start++;
+    }, time);
+}
+userProgress(3);
+
+function userProgressCallBack() {
+    document.querySelector('.hidden-block').style.display = 'block';
+}
